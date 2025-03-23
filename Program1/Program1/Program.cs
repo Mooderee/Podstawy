9ca22c2b9b1966dd5b8160bd14b1db386f916b00
Moodere <koszmor@gmail.com>
2025-03-23 16:19:58 +0100

    test

---
+++ Program1/Program1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Program1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj 2 liczby dodatnie");
+            int liczba1 = int.Parse(Console.ReadLine());
+            int liczba2 = int.Parse(Console.ReadLine());
+
+
+            while (liczba1 < 0 || liczba2 < 0)
+            {
+                if (liczba1 < 0)
+                {
+                    Console.WriteLine("Zła liczba, podaj pirerwszą liczbę dodatnią:");
+                    liczba1 = int.Parse(Console.ReadLine());
+                }
+                else if (liczba2 < 0)
+                {
+                    Console.WriteLine("Zła liczba, podaj drugą liczbę dodatnią:");
+                    liczba2 = int.Parse(Console.ReadLine());
+                }
+            }
+
+            // Obliczenie średniej arytmetycznej:
+                    double srednia = (liczba1 + liczba2) / 2.0;
+
+            // Wyświetlenie wyniku:
+            Console.WriteLine("Średnia arytmetyczna to: " + srednia);
+        }
+    }
+}
